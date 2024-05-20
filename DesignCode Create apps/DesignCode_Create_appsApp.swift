60c0c7f68aba5ea39d17f7d907db676f8d6a0c74
Adam Kaum <adam.bolotov@gmail.com>
2024-05-20 07:21:13 +0400

    Initial Commit

---
+++ DesignCode Create apps/DesignCode_Create_appsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode_Create_appsApp.swift
+//  DesignCode Create apps
+//
+//  Created by Adam Kaum on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode_Create_appsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
